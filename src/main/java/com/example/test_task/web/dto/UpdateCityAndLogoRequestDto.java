d645818b73908552d5c3e04ac4964e1f4e9c7a7a
Vlad Kovalev <kovalevvvlad981@gmail.com>
2023-08-21 18:44:50 +0300

    fix swagger bug

--- src/main/java/com/example/test_task/web/dto/UpdateCityAndLogoRequestDto.java
+++ src/main/java/com/example/test_task/web/dto/UpdateCityAndLogoRequestDto.java
@@ -10,13 +10,13 @@ import javax.validation.constraints.NotNull;
 @Data
 @Schema(description = "dto for updating city and its logo")
 public class UpdateCityAndLogoRequestDto {
-    @Schema(name = "current city name", example = "Minsk")
+    @Schema(name = "currentCityName", example = "Minsk")
     @NotBlank
     private String currentCityName;
-    @Schema(name = "new city name", example = "Mensk")
+    @Schema(name = "newCityName", example = "Mensk")
     @NotBlank
     private String newCityName;
-    @Schema(name = "file with new logo")
+    @Schema(name = "file")
     @NotNull
     private MultipartFile file;
 }
